97d3e7242db7bd1f1b20cdc2d21a4aa86d210e0e
Sakhile Mkhalele <125193397+SakhileM17@users.noreply.github.com>
2024-04-03 14:35:12 +0200

    Initial commit

--- server/db/connect.js
+++ server/db/connect.js
@@ -3,11 +3,11 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const pool = new Pool({
 
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
+  user: process.env.DB_USER, // database username
+  password: process.env.DB_PASSWORD, // dataebase password
+  host: process.env.DB_HOST, //database host
+  port: process.env.DB_PORT, // database port
+  database: process.env.DB_DATABASE // database name
 
 });
 
